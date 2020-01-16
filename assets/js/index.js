bad2ccb56c715b72d287be0a7f59614b13921b2f
bcrist-qualitylogic <bcrist@qualitylogic.com>
2020-01-15 17:24:05 -0700

    Edited the mobile detection js file a bit.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,24 +10,28 @@ function detectMobileByResolution() {
 }
 
 function detectMobileByAgent() { 
-    if( navigator.userAgent.match(/Android/i)
+    if
+    (navigator.userAgent.match(/Android/i)
     || navigator.userAgent.match(/webOS/i)
     || navigator.userAgent.match(/iPhone/i)
     || navigator.userAgent.match(/iPad/i)
     || navigator.userAgent.match(/iPod/i)
     || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ){
+    || navigator.userAgent.match(/Windows Phone/i))
+    {
         return true;
     }
-    else {
+    else 
+    {
         return false;
     }
 }
 
 if (detectMobileByResolution() || detectMobileByAgent())
 {
-  document.getElementById("mainSect").style.fontSize = "3em";
-  document.getElementById("mainSect").style.display = "flex";
-  document.getElementById("mainSect").style.overflow = "auto";
+    document.getElementById("mainSect").style.fontSize = "3em";
+}
+else 
+{
+    document.getElementById("mainSect").style.fontSize = "1.25em";
 }
