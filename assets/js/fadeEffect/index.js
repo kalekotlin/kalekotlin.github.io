51d222c1bf9c107dfbf0a44e557e61c949b75c18
mochamethod <mochamethod@gmail.com>
2020-01-18 18:31:10 -0700

    Added icons and assets. Goofed up the blog posts though...

--- assets/js/fadeEffect/index.js
+++ assets/js/fadeEffect/index.js
@@ -9,7 +9,21 @@ function fadeIn() {
         element[0].style.opacity = op;
         element[0].style.filter = 'alpha(opacity=' + op * 100 + ")";
         op += 0.1;
-    }, 50);
+    }, 75);
+
+    var nav = document.getElementById("navigationElement");
+    var timer = setInterval(function () {
+        if (op > 1){
+            clearInterval(timer);
+        }
+        nav.style.opacity = op;
+        nav.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += 0.1;
+    }, 5);
 }
 
 fadeIn();
+document.getElementById("navigationElement").style.display = "flex";
+document.getElementById("navigationElement").style.justifyContent = "flex-end";
+document.getElementById("navigationElement").style.flexDirection = "row";
+document.getElementById("navigationElement").style.position = "fixed";
