30b7ed35ef2cd29952c8dc55a41f751bd058ee85
mochamethod <mochamethod@gmail.com>
2020-02-01 22:17:33 -0700

    Fixed up the contact page a bit. Still needs some work though.

--- assets/js/core/validateInput.js
+++ assets/js/core/validateInput.js
@@ -8,12 +8,14 @@ function validateInput()
     var timer = setInterval(function () {
         if (nameInput.value != "" && emailInput.value != "" && messageInput.value != "")
         {
-            button.style.boxShadow = "3px 4px 0px 0px rgb(117, 173, 152)";
-            button.style.backgroundColor = "#98E2C6";
-            button.style.border = "1px solid rgb(134, 202, 176)";
-            button.style.textShadow = "0px 1px 0px rgb(138, 206, 180)";
+            button.style.boxShadow = "3px 4px 0px 0px #DB5C3D";
+            button.style.backgroundColor = "#F06543";
+            button.style.border = "1px solid #DB5C3D";
             button.style.cursor = "pointer";
-            button.style.color = "#545c52";
+            button.style.color = "#313638";
+            button.style.padding = "1em";
+            button.style.margin = "1em";
+            button.style.minWidth = "8em";
             button.disabled = false;
         }  
         else if (nameInput.value === "" || emailInput.value === "" || messageInput.value === "")
@@ -21,9 +23,11 @@ function validateInput()
             button.style.boxShadow = "3px 4px 0px 0px #333832";
             button.style.backgroundColor = "#484948";
             button.style.border = "1px solid #545c52";
-            button.style.textShadow = "0px 1px 0px #545c52";
             button.style.cursor = "default";
             button.style.color = "#545c52";
+            button.style.padding = "1em";
+            button.style.margin = "1em";
+            button.style.minWidth = "8em";
             button.disabled = true;
         }
     }, 200);
