78b3463ab85fbbb7f9fc3d36f5fb9b3b7dd984b2
mochamethod <mochamethod@gmail.com>
2020-02-02 12:18:47 -0700

    Improved styling of the blog page, as well as small styling tweaks to the home and contact pages.

--- assets/js/core/validateInput.js
+++ assets/js/core/validateInput.js
@@ -1,3 +1,31 @@
+function changeButtonState(button, isButtonEnabled)
+{
+    if (isButtonEnabled)
+    {
+        button.style.boxShadow = "3px 4px 0px 0px #DB5C3D";
+        button.style.backgroundColor = "#F06543";
+        button.style.border = "1px solid #DB5C3D";
+        button.style.cursor = "pointer";
+        button.style.color = "#313638";
+        button.style.padding = "1em";
+        button.style.margin = "1em";
+        button.style.minWidth = "8em";
+        button.disabled = false;
+    }
+    else
+    {
+        button.style.boxShadow = "3px 4px 0px 0px #333832";
+        button.style.backgroundColor = "#484948";
+        button.style.border = "1px solid #545c52";
+        button.style.cursor = "default";
+        button.style.color = "#545c52";
+        button.style.padding = "1em";
+        button.style.margin = "1em";
+        button.style.minWidth = "8em";
+        button.disabled = true;
+    }
+}
+
 function validateInput()
 {
     var nameInput = document.getElementById("contactInputName");
@@ -8,27 +36,19 @@ function validateInput()
     var timer = setInterval(function () {
         if (nameInput.value != "" && emailInput.value != "" && messageInput.value != "")
         {
-            button.style.boxShadow = "3px 4px 0px 0px #DB5C3D";
-            button.style.backgroundColor = "#F06543";
-            button.style.border = "1px solid #DB5C3D";
-            button.style.cursor = "pointer";
-            button.style.color = "#313638";
-            button.style.padding = "1em";
-            button.style.margin = "1em";
-            button.style.minWidth = "8em";
-            button.disabled = false;
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (re.test(String(emailInput.value).toLowerCase())) 
+            {
+                changeButtonState(button, true);
+            }
+            else
+            {
+                changeButtonState(button, false);
+            }
         }  
         else if (nameInput.value === "" || emailInput.value === "" || messageInput.value === "")
         {
-            button.style.boxShadow = "3px 4px 0px 0px #333832";
-            button.style.backgroundColor = "#484948";
-            button.style.border = "1px solid #545c52";
-            button.style.cursor = "default";
-            button.style.color = "#545c52";
-            button.style.padding = "1em";
-            button.style.margin = "1em";
-            button.style.minWidth = "8em";
-            button.disabled = true;
+            changeButtonState(button, false);
         }
     }, 200);
 }
--- assets/js/fadeEffect/index.js
+++ assets/js/fadeEffect/index.js
@@ -1,15 +1,15 @@
 function fadeIn() {
     var op = 0;
-    var element = document.getElementsByClassName("mainContent");
+    var element = document.getElementById("mainContent");
 
     var timer = setInterval(function () {
         if (op >= 1) {
             clearInterval(timer);
         } 
-        element[0].style.opacity = op;
-        element[0].style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op += 0.1;
-        if (window.getComputedStyle(element[0]).getPropertyValue("opacity") > 1) {
+        if (window.getComputedStyle(element).getPropertyValue("opacity") > 1) {
             return;
         }
     }, 25);
